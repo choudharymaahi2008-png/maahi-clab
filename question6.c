561f22f55de2914dfb3221d27f9bc3f513f4ca03
choudharymaahi2008-png <choudharymaahi2008@gmail.com>
2026-08-19 23:40:52 +0530

    Add files via upload

---
+++ question6.c
@@ -0,0 +1,20 @@
+//write  a program  to swap two numbers using a third variable.
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+a= 5;
+b= 10;
+
+c=a;
+b=c;
+a= b+c;
+
+printf("New value of a is : %d\n " ,a);
+printf("New value of b is : %d\n " ,b);
+
+return 0;
+
+}
+
+
